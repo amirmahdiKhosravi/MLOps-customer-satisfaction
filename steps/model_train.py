169282d97cfce6e743be2d6f495ab20665c02208
Amirmahdi <a.khosravi.tabrizi@gmail.com>
2023-12-19 20:42:10 -0500

    model_train in steps completed and config.py added.

--- steps/model_train.py
+++ steps/model_train.py
@@ -1,13 +1,32 @@
 import logging
 from zenml import step
 import pandas as pd
+from src.model_dev import LinearRegressionModel
+from sklearn.base import RegressorMixin
+from .config import ModelNameConfig
 
 @step
-def train_model(df: pd.DataFrame) -> None:
+def train_model(
+    X_train: pd.DataFrame,
+    X_test: pd.DataFrame,
+    y_train: pd.DataFrame,
+    y_test: pd.DataFrame, 
+    config: ModelNameConfig
+) -> RegressorMixin:
     """
     Training the model on the given dataframe.
 
     Args: 
         df: the given dataframe for training.
     """
-    pass
+    try:
+        model = None
+        if config.model_name == "LinearRegression":
+            model = LinearRegressionModel(X_train, y_train)
+            trained_model = model.train()
+            return trained_model
+        else:
+            raise ValueError("Model {} not supported".format(config.model_name))
+    except Exception as e:
+        logging.error(f"Error in train_model: {e}")
+        raise e
